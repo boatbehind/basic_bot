6ea087e02710639479b4f7d7b9c3c0349df07bca
YOUR NAME <boatbehind0430@gmail.com>
2017-09-24 17:07:00 -0400

    a

--- app.rb
+++ app.rb
@@ -7,8 +7,6 @@ enable :sessions
 
 @client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
 
-
-
 configure :development do
   require 'dotenv'
   Dotenv.load
@@ -30,30 +28,56 @@ get "/sms/incoming" do
   body = params[:Body] || ""
   body = body.downcase.strip
 
-
   if session["counter"] == 1
-     message = "Hey! I'm Lumix, I could help you order movie ticekts. "
-  elsif body == "Hi" || body == "Hello" || body == "Hey"
-    message = " Hey! Tell me what movie do you like?"
-  elsif body == "War movie"
-    message = "We have Dunkirk and Hacksaw Ridge"
-  elsif body == "Dunkirk"
-      message = "Coll! This is Nolan's new movie. It talks about firece battel in World War2. When do you want to see it? "
-  elsif body == "Tommorrow 5 pm"
-    message = "Cool! The Manor will have a movie that time."
-  elsif body == "How much is it"
-      message = "8$, book right now?"
-  elsif body = "OK"
-    message = "Great! I just booked, do you want add it to calender?"
-  elsif body = "Thanks"
-    message = "You are welcome! I'm happy to talk aout this movie after you finishing it!"
+    message = "Well, hey there! I'm Michael. You ready to get started? Type menu to choose what you want to know"
+    #media = "https://media.giphy.com/media/13ZHjidRzoi7n2/giphy.gif"
+  elsif body == "hello" || body == "hi" || body == "hey"
+    message = "Hey there!"
+  elsif body == "menu"
+    message = "Type any of the following to learn about me! Name, age, height, college career, career goals, interests"
+  elsif body == "name"
+    message = "Michael Benjamin Brough"
+  elsif body == "age"
+    message = "25"
+  elsif body == "height"
+    message = "6' even"
+  elsif body == "college career"
+    message = "I graduated from BYU in April 2017 with a bachelors in Mechanical Engineering and a 3.84 GPA"
+  elsif body == "career goals"
+    message = "I want to get involved in the product development of electromechanical devices used in everyday situations"
+  elsif body == "interests"
+    message = "I love playing volleyball, soccer, being social with friends, and watching movies"
+  else
+    message = "I'm sorry, I didn't get that"
+    media = nil
   end
 
-   session["counter"] += 1
+  # if body == "hello" || body == "hi" || body =="hey"
+  #   message = "Well, hey there!"
+  #   elsif body == "who"
+  #     message = "I'm Daragh's MeBot"
+  #   elsif body == "what"
+  #       message = "I'm a bot that'll let you ask things about Daragh without bothering him."
+  #   elsif body == "why"
+  #     message = "He made me for this class. To show you how to make simple bots"
+  #   elsif body == "where"
+  #     message = "I'm on a server in the cloud.. But Daragh's in Pittsburgh"
+  #   elsif body == "when"
+  #     message = "I was made on Sept 14th. But Daragh is much older than that"
+  #   else
+  #     message = "I didn't understand that. You can say who, what, where, when and why?"
+  #
+  #   end
+
+
+  session["counter"] += 1
 
   twiml = Twilio::TwiML::MessagingResponse.new do |r|
     r.message do |m|
       m.body( message )
+      unless media.nil?
+        m.media( media )
+      end
     end
   end
 
