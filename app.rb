f4ddadd93c76c658cf8ba919bfcfaaab0c8c1cbe
YOUR NAME <boatbehind0430@gmail.com>
2017-10-19 11:59:49 -0400

    test5

--- app.rb
+++ app.rb
@@ -39,16 +39,22 @@ get "/sms/incoming" do
     message = "Yeah! Tell me the movie name you like."
   elsif body == "forrest gump"
     message =
-"🎫vote average:8.2
-🕰release year:1994
-🎬cast:Tom Hanks, Robin Wright
-🔖overview:A man with a low IQ has accomplished great things in his life and been present during significant historic events - in each case, far exceeding what anyone imagined he could do. Yet, despite all the things he has attained, his one true love eludes him. 'Forrest Gump' is the story of a man who rose above his challenges, and who proved that determination, courage, and love are more important than ability."
+"
+🎫vote average: 8.2
+🕰release year: 1994
+🎬cast: Tom Hanks, Robin Wright
+📘overview: A man with a low IQ has accomplished great things in his life and been present during significant historic events - in each case, far exceeding what anyone imagined he could do. Yet, despite all the things he has attained, his one true love eludes him. 'Forrest Gump' is the story of a man who rose above his challenges, and who proved that determination, courage, and love are more important than ability."
   elsif body == "the foreigner"
     message =
-"🎫vote average:6.3
-🕰release year:2017
-🎬cast:Jackie Chan, Pierce Brosnan, Charlie Murphy
-🔖overview:Quan is a humble London businessman whose long-buried past erupts in a revenge-fueled vendetta when the only person left for him to love -- his teenage daughter -- dies in a senseless act of politically motivated terrorism. His relentless search to find the terrorists leads to a cat-and-mouse conflict with a British government official whose own past may hold the clues to the identities of the elusive killers.
+"
+🎫vote average: 6.3
+
+🕰release year: 2017
+
+🎬cast: Jackie Chan, Pierce Brosnan, Charlie Murphy
+
+📘overview: Quan is a humble London businessman whose long-buried past erupts in a revenge-fueled vendetta when the only person left for him to love -- his teenage daughter -- dies in a senseless act of politically motivated terrorism. His relentless search to find the terrorists leads to a cat-and-mouse conflict with a British government official whose own past may hold the clues to the identities of the elusive killers.
+
 This movie is on! Do you wanna see it?"
   elsif body == "yes"
     message = "where is you location?"
