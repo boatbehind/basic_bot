20132d9d8477b78b6a835b001f58b36afdbc62a4
YOUR NAME <boatbehind0430@gmail.com>
2017-10-19 11:22:59 -0400

    test2

--- app.rb
+++ app.rb
@@ -35,12 +35,13 @@ get "/sms/incoming" do
 
   if session["counter"] == 1
     message = "Hey there, I'm Lumimd. I can share any information about any movie!"
-  elsif body == "hello" || body == "hi" || body == "hey"|| body == "cool"
+  elsif body == "great" || body == "cool"
     message = "Yeah! Tell me the movie name you like."
   elsif body == "forrest gump"
     message = "vote average:8.2" + "release year:1994" + "cast:Tom Hanks, Robin Wright" +"overview:A man with a low IQ has accomplished great things in his life and been present during significant historic events - in each case, far exceeding what anyone imagined he could do. Yet, despite all the things he has attained, his one true love eludes him. 'Forrest Gump' is the story of a man who rose above his challenges, and who proved that determination, courage, and love are more important than ability."
   elsif body == "the foreigner"
-    message = "vote average:6.3" + "release year:2017" + "cast:Jackie Chan, Pierce Brosnan, Charlie Murphy" +"overview:Quan is a humble London businessman whose long-buried past erupts in a revenge-fueled vendetta when the only person left for him to love -- his teenage daughter -- dies in a senseless act of politically motivated terrorism. His relentless search to find the terrorists leads to a cat-and-mouse conflict with a British government official whose own past may hold the clues to the identities of the elusive killers." + "This movie is on! Do you wanna see it?"
+    message = "https://www.themoviedb.org/movie/379149-the-foreigner" + "vote average:6.3
+    release year:2017"  + "cast:Jackie Chan, Pierce Brosnan, Charlie Murphy" +"overview:Quan is a humble London businessman whose long-buried past erupts in a revenge-fueled vendetta when the only person left for him to love -- his teenage daughter -- dies in a senseless act of politically motivated terrorism. His relentless search to find the terrorists leads to a cat-and-mouse conflict with a British government official whose own past may hold the clues to the identities of the elusive killers." + "This movie is on! Do you wanna see it?"
   elsif body == "yes"
     message = "where is you location?"
   elsif body == "akron"
@@ -50,7 +51,7 @@ get "/sms/incoming" do
   elsif body == "1"
     message ="What date are you looking for? (MM/DD/YYYY)"
   elsif body == "10/25/2017"
-    message ="Here we get some shows on 10/25/2017 at AMC Waterfront 22. Please check your tickets in this page. https://www.fandango.com/amc-waterfront-22-aaosa/theater-page?date=2017-10-25"
+    message ="Here we get some shows on 10/25/2017 at AMC Waterfront 22. Please check your tickets in this page. https://tickets.fandango.com/MobileExpress/Checkout?row_count=198402646&tid=aaosa&mid=193787&chainCode=AMC&source=MobileWeb&sdate=2017-10-25%2013:35"
   else
     message = "I'm sorry, I didn't get that"
     media = nil
